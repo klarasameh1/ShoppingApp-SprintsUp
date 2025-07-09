2d4485ba96b7dee644ecd3f0b52a16d1f933ee94
klara sameh <klarasameh39@gmail.com>
2025-07-09 19:58:49 +0300

    Added checking Out

--- lib/pages/shopping_cart.dart
+++ lib/pages/shopping_cart.dart
@@ -19,8 +19,9 @@ class _ShoppingCartState extends State<ShoppingCart> {
       body: Column(
         children: [
           Center(
-            child: Icon(Icons.shopping_cart_outlined, size: 80, color: Color(0xFFB284BE)),
+            child: Icon(Icons.shopping_cart, size: 80, color: Color(0xFFB284BE)),
           ),
+          SizedBox(height: 15,),
           Expanded(
             child: ListView.builder(
               itemCount: cartList.length,
@@ -34,7 +35,7 @@ class _ShoppingCartState extends State<ShoppingCart> {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       IconButton(
-                        icon: Icon(Icons.remove_circle_outline),
+                        icon: Icon(Icons.remove_outlined),
                         onPressed: () {
                           setState(() {
                             if (item.quantity > 1) {
@@ -47,7 +48,7 @@ class _ShoppingCartState extends State<ShoppingCart> {
                       ),
                       Text('${item.quantity}' , style: TextStyle(fontSize: 18 ),),
                       IconButton(
-                        icon: Icon(Icons.add_circle_outline),
+                        icon: Icon(Icons.add_outlined),
                         onPressed: () {
                           setState(() {
                             item.quantity++;
@@ -60,8 +61,55 @@ class _ShoppingCartState extends State<ShoppingCart> {
               },
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Total:", style: TextStyle(fontSize: 20)),
+                Text("${calcTotal().toStringAsFixed(2)} EGP",
+                    style: TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: ElevatedButton(
+              onPressed: () {
+
+                if(cartList.isEmpty){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Your Cart Is Empty")),
+                  );
+                }
+                else{
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Ordered Successfully :)")),
+                  );
+                  cartList.clear();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                }
+
+              },
+
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Color(0xFFA361AE)) ,),
+              child: Text("Proceed to Checkout", style: TextStyle(fontSize:18 , color: Colors.black)),
+            ),
+          )
         ],
       ),
     );
   }
+  double calcTotal(){
+    double total = 0.0 ;
+    for (CartItem item in cartList){
+      total += item.product.price * item.quantity ;
+    }
+    return total;
+  }
+
 }
